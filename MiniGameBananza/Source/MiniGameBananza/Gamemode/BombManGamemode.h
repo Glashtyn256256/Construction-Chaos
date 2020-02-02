bebe8ef6d1e9f535cb78f8bddfe0f581acafacf6
tommohawkaction <tom1998dunn@hotmail.com>
2020-02-02 13:47:03 +0000

    [CODE] Player should now be physics based and move correctly

---
+++ MiniGameBananza/Source/MiniGameBananza/Gamemode/BombManGamemode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGameBananzaGameModeBase.h"
+#include "BombManGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIGAMEBANANZA_API ABombManGamemode : public AMiniGameBananzaGameModeBase
+{
+	GENERATED_BODY()
+	
+		
+};
