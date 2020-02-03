c1c5b17045bd5d1c21eee5ace2b15f391b705460
Adam <adampusey@hotmail.co.uk>
2020-02-03 14:01:12 +0000

    Implemented arming, defusing and reseting functions for bomb (timer)

--- MiniGameBananza/Source/MiniGameBananza/BombMan/BombManBomb.cpp
+++ MiniGameBananza/Source/MiniGameBananza/BombMan/BombManBomb.cpp
@@ -9,7 +9,11 @@ ABombManBomb::ABombManBomb()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	if (StaticMeshComponent)
+	{
+		StaticMeshComponent->AttachTo(RootComponent);
+	}
 
 }
 
@@ -17,7 +21,7 @@ ABombManBomb::ABombManBomb()
 void ABombManBomb::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -25,5 +29,23 @@ void ABombManBomb::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (IsArmed)
+	{
+		TimeUntilDetonation -= DeltaTime;
+	}
 }
 
+void ABombManBomb::ArmBomb()
+{
+	IsArmed = true;
+}
+
+void ABombManBomb::DefuseBomb()
+{
+	IsArmed = false;
+}
+
+void ABombManBomb::ResetBomb()
+{
+	TimeUntilDetonation = DetonationTime;
+}
