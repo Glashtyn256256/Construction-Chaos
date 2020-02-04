b4f59f7873b56e03031f5f839290ab1fc2265d6d
Glashtyn256256 <recovery903@gmail.com>
2020-02-04 11:47:16 +0000

    [CODE] Ben - Explosion movement now working correctly and stops at the correct place.

--- MiniGameBananza/Source/MiniGameBananza/BombMan/BombManExplosion.cpp
+++ MiniGameBananza/Source/MiniGameBananza/BombMan/BombManExplosion.cpp
@@ -1,35 +1,49 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Engine/World.h"
+#include "Engine/Engine.h"
 #include "BombManExplosion.h"
 
 // Sets default values
-ABombManExplosion::ABombManExplosion()
+ABombManExplosion::ABombManExplosion() : StopExplosion(false)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
 	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Component"));
 	if (BoxComponent) {
 		BoxComponent->AttachTo(RootComponent);
 	}
-
+	
 	ParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Particle System Component"));
 	if (ParticleSystem) {
-		ParticleSystem->AttachTo(BoxComponent);
+		ParticleSystem->AttachTo(RootComponent);
 	}
-
 }
 
 // Called when the game starts or when spawned
 void ABombManExplosion::BeginPlay()
 {
-	Super::BeginPlay();
 	
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();	
 }
 
 // Called every frame
 void ABombManExplosion::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (StopExplosion == false)
+	{
+		FVector NewLocation = GetActorForwardVector() * (Speed * DeltaTime);
+		AddActorWorldOffset(NewLocation, true);
+		FVector VectorLength = StartLocation - GetActorLocation();
+
+		if (VectorLength.Size() >= Length)
+		{
+			StopExplosion = true;
+		}
+	}
 }
 
