dd3b811964503ab7a9ffb87ba408ffef93768c24
Benjamin Moore <recovery903@gmail.com>
2020-02-17 15:58:42 +0000

    [CODE/UI] Ben - Code now works when we click a button in the UI it loads a new level.

--- MiniGameBananza/Source/MiniGameBananza/UI/MainMenu/UI_MainMenu.cpp
+++ MiniGameBananza/Source/MiniGameBananza/UI/MainMenu/UI_MainMenu.cpp
@@ -1,5 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 #include "UI_MainMenu.h"
 
+void UUI_MainMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (StartGameButton) 
+	{
+		StartGameButton->OnClicked.AddDynamic(this, &UUI_MainMenu::OnClickStartButton);
+	}
+}
+
+void UUI_MainMenu::OnClickStartButton() 
+{
+	const UWorld* World = GetWorld();
+
+	if (World)
+	{
+		UGameplayStatics::OpenLevel(World, FName(TEXT("Test")));
+	}
+}
--- MiniGameBananza/Source/MiniGameBananza/UI/MainMenu/UI_MainMenu.h
+++ MiniGameBananza/Source/MiniGameBananza/UI/MainMenu/UI_MainMenu.h
@@ -4,21 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Engine/World.h"
 #include "Components/Button.h"
 #include "UI_MainMenu.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class MINIGAMEBANANZA_API UUI_MainMenu : public UUserWidget
 {
 	GENERATED_BODY()
 	
-public: 
-	UPROPERTY(EditDefaultsOnly,meta = (BindWidget))
+public:
+	virtual void NativeConstruct() override;
+	UFUNCTION()
+	void OnClickStartButton();
+public:
+	//TSubclassOf<class UUserWidget> 
+
+	UPROPERTY(EditAnywhere,meta = (BindWidget))
 	UButton* StartGameButton;
-
-
-
 };
