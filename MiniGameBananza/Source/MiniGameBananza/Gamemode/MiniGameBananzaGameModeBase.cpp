cdf97f8ddd64fcc474c08342f505c85b522ec0b3
tommohawk1998 <tomdunn1998@hotmail.co.uk>
2020-02-24 15:18:24 +0000

    [CODE] Added seprated UI for each Player Controller and they all share a static HUD. Have also added Player lives to the UI

--- MiniGameBananza/Source/MiniGameBananza/Gamemode/MiniGameBananzaGameModeBase.cpp
+++ MiniGameBananza/Source/MiniGameBananza/Gamemode/MiniGameBananzaGameModeBase.cpp
@@ -2,4 +2,18 @@
 
 
 #include "MiniGameBananzaGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
 
+void AMiniGameBananzaGameModeBase::InitGame(const FString & MapName, const FString & Options, FString & ErrorMessage)
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+
+	UWorld* const world = GetWorld();
+	if (world)
+	{
+		for (int i = 0; i <= PlayerCount; ++i)
+		{
+			UGameplayStatics::CreatePlayer(world, i);
+		}
+	}
+}
