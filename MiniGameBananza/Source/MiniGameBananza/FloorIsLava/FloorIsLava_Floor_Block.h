7a812af7b9b39c6b9f9129d8e552fbaeba08310c
Recov <Recov@LAPTOP-5SU6BEGF>
2020-04-20 23:37:59 +0100

    [CODE] Ben - Floor is lava block created, test in place that toggles the simulation physics on to drop cube.

---
+++ MiniGameBananza/Source/MiniGameBananza/FloorIsLava/FloorIsLava_Floor_Block.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "FloorIsLava_Floor_Block.generated.h"
+
+UCLASS()
+class MINIGAMEBANANZA_API AFloorIsLava_Floor_Block : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloorIsLava_Floor_Block();
+
+	void TogglePhysicsSimulation();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* MeshComponent;
+	
+	int test = 0;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	//virtual void SetSimulatePhysics();
+
+};
