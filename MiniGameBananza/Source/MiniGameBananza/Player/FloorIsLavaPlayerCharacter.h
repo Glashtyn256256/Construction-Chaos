dc2c020199e62e73776e0aaa81dd40d5f3f50ecb
tommohawkaction <tom1998dunn@hotmail.com>
2020-04-27 10:49:43 +0100

    [CODE] Some work on the floor is lava gamemode -> Pushing players

--- MiniGameBananza/Source/MiniGameBananza/Player/FloorIsLavaPlayerCharacter.h
+++ MiniGameBananza/Source/MiniGameBananza/Player/FloorIsLavaPlayerCharacter.h
@@ -24,6 +24,7 @@ public:
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
 	virtual FVector GetAnimVelocity() const override;
+	void AddForceVelocity(FVector Force);
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
@@ -45,4 +46,5 @@ public:
 private:
 	float SphereRadius, Timer, MaxTimer;
 	FVector Velocity;
+	FVector Force;
 };
