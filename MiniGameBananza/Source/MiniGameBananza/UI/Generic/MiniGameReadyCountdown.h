3c2ffae407b3ef0dbdd198a4bd0a18d81bddd5cc
Glashtyn256256 <recovery903@gmail.com>
2020-05-03 19:12:27 +0100

    [Merge] Ben - Not sure why it won't merge have to do it this way.

--- MiniGameBananza/Source/MiniGameBananza/UI/Generic/MiniGameReadyCountdown.h
+++ MiniGameBananza/Source/MiniGameBananza/UI/Generic/MiniGameReadyCountdown.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "Components/Image.h"
+#include "Engine/Classes/Sound/SoundCue.h"
 #include "MiniGameReadyCountdown.generated.h"
 
 enum class ECountdownState
@@ -29,6 +30,13 @@ public:
 
 	void SetCountdownState(ECountdownState state);
 	bool NextCountdownState(ECountdownState& state);
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundCue* scTick;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundCue* scGo;
 
 protected:
 	UPROPERTY(EditAnywhere, meta = (BindWidget))
